727b04065b27698afc3a36fec58c809764ec72ff
Carl Kho <106736711+CarlKho-Minerva@users.noreply.github.com>
2024-05-24 20:30:35 +0800

    feat: Add CORS configuration and API endpoints for students
    This commit adds the necessary CORS configuration to allow all origins and includes two API endpoints for managing student data. The first endpoint handles both creating and listing students, while the second endpoint allows for updating and deleting individual student records. Additionally, the commit includes the necessary files and configurations for the Django project.

--- django_react_proj/django_react_proj/urls.py
+++ django_react_proj/django_react_proj/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from students import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+
+    # The first endpoint will handle both creations (POST) and listing (GET).
+    re_path(r'^api/students/$', views.students_list),
+
+    #The second one will remove (DELETE) or update (PUT) the data of a single student.
+    re_path(r'^api/students/([0-9])$', views.students_detail)
 ]
